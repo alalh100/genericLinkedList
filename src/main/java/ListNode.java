244fd50a3da18ba4243302e12bd5b4ae99adf39c
alalh100 <alalh100@hhu.de>
2019-04-22 22:05:21 +0200

    sorting while inserting with bug

--- src/main/java/ListNode.java
+++ src/main/java/ListNode.java
@@ -3,22 +3,32 @@ public class ListNode<T extends Comparable<T> > {
     private Node<T> head = null;
 
     private ListNode( Node<T> head){
+
         this.head = head;
     }
+/*
+    private void insert( Node<T> newNode ){
 
+            newNode.next = head;
+            head = newNode;
+    }
+*/
     private void insert( Node<T> newNode ){
 
         Node<T> current = head;
+        if (newNode.element.compareTo( current.element) <= 0 ){
+            newNode.next = head;
+            head = newNode;
+        }else {
 
-        if ( newNode instanceof Comparable ){
+            while (newNode.element.compareTo(current.element) > 0 && current.next != null) {
 
-            while ( newNode.element.compareTo( current.element) <= 0 && current != null  ){
                 current = current.next;
+
             }
-            newNode.next = current;
-
+            newNode.next = current.next;
+            current.next = newNode;
         }
-
 
     }
 
@@ -30,6 +40,7 @@ public class ListNode<T extends Comparable<T> > {
             System.out.println("   Element=" + current.element);
             current = current.next;
         }
+        System.out.println("head ist "+ head.element);
     }
 
     public static void main(String[] args) {
@@ -37,6 +48,13 @@ public class ListNode<T extends Comparable<T> > {
         ListNode<Integer> root = new ListNode<>(new Node<> (5 ) );
         root.insert(new Node<> (6));
         root.insert(new Node<> (4));
+        root.insert(new Node<> (3));
+        root.insert(new Node<> (2));
+        root.insert(new Node<> (7));
+        root.insert(new Node<> (9));
+        root.insert(new Node<> (12));
+        root.insert(new Node<> (10));
+        root.insert(new Node<> (1));
         root.print();
     }
 
